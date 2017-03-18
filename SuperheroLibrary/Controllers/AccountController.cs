e9f4d39854fcadf8a386151740b876285eeaa3ee
mikhailkovalev <mixon271@gmail.com>
2017-03-18 10:55:39 +0400

    authorization was appended

---
+++ SuperheroLibrary/Controllers/AccountController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SuperheroLibrary.Models;
+
+namespace SuperheroLibrary.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                using (AppContext db = new AppContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Login == model.Name && u.Password == model.Password);
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                using (AppContext db = new AppContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Login == model.Name);
+                }
+                if (user == null)
+                {
+                    // создаем нового пользователя
+                    using (AppContext db = new AppContext())
+                    {
+                        db.Users.Add(new User { Login = model.Name, Password = model.Password});
+                        db.SaveChanges();
+
+                        user = db.Users.Where(u => u.Login == model.Name && u.Password == model.Password).FirstOrDefault();
+                    }
+                    // если пользователь удачно добавлен в бд
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Name, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
